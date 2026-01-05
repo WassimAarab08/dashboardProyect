b1d512045f355a2d1e0f9dd5ca9df2d0cce6d61e
wassi <132829535+WassimAarab08@users.noreply.github.com>
2026-01-05 14:45:42 +0100

    Agregar soporte para la vinculación de eventos y mejorar el diseño del panel de control; se incluye un nuevo ícono de Angular y se ajustan los estilos de la barra superior.

--- src/app/features/dashboard/pages/data-binding-page/data-binding-page.ts
+++ src/app/features/dashboard/pages/data-binding-page/data-binding-page.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-data-binding',
   imports: [],
   templateUrl: './data-binding-page.html',
-  
 })
-export default class DataBindingComponent { }
+export default class DataBindingComponent {
+  propertyBinding: string = '/assets/icons/angular.svg';
+  eventBinding = signal<number>(0);
+
+  sumar(): void {
+    this.eventBinding.update((n) => n + 1);
+  }
+}
